fd4304aa1391e9649d74e74fac5477d1b135a20c
Muhammed Elfeqy <osclancer@gmail.com>
2022-06-29 01:36:48 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+/**
+ * Main Template file
+ * 
+ * @package Handed
+ */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Handed  Theme</title>
+</head>
+<body>
+    <h1>This Is the theme</h1>    
+</body>
+</html>
